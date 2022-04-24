549c54c2627a3a6c3f0a5210c6ab52798e837d7c
dao vo <gary.vo32@gmail.com>
2022-04-23 18:30:36 -0700

    finished UI of PersonalInfo

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -2,10 +2,22 @@ import React from "react";
 
 const PersonalInfo = () => {
   return (
-    <div>
+    <div className="personal">
       <h2>PersonalInfo</h2>
-      <label htmlFor="name">First Name</label>
-      <input type="text" placeholder="first name" id="name" />
+      <label htmlFor="firstName">First Name</label>
+      <input type="text" placeholder="first name" id="firstName" />
+
+      <label htmlFor="lastName">Last Name</label>
+      <input type="text" placeholder="last name" id="lastName" />
+
+      <label htmlFor="personalDesc">Personal Description</label>
+      <input type="text" placeholder="brief description" id="personalDesc" />
+
+      <label htmlFor="email">e-mail</label>
+      <input type="email" placeholder="sample@here.aol" id="email" />
+
+      <label htmlFor="phone">Personal Description</label>
+      <input type="tel" placeholder="XXX-XXX-XXXX" id="phone" />
     </div>
   );
 };
