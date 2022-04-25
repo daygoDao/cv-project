a2faa5ed68e33e0e8629f5190135b58c53bad1b8
dao vo <gary.vo32@gmail.com>
2022-04-24 20:35:18 -0700

    able to display hardcoded personal obj to preview section

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 const Preview = ({person}) => {
-  
-  console.log(person)
+  const {firstName, lastName, personalDesc, email,phone} = person;
+  console.log(firstName)
   return (
     <div className='cvPreview'>
         <section className='cvPersonalDesc'>
             <h4>teset</h4>
-            <h5>yo, {}</h5>
+            <h5>{firstName} {lastName}</h5>
+            <p>email: {email}</p>
+            <p>Phone: {phone}</p>
+            <p>About me: {personalDesc}</p>
         </section>
     </div>
   )
